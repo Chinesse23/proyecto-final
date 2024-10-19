88325e8ca39ef657be01b82f93a18f3ad170872a
Chinesse23 <czuazua18@gmail.com>
2024-10-18 16:51:44 -1100

    nuevos cambios en los archivos

--- middleware/verifyRecaptchaToken.js
+++ middleware/verifyRecaptchaToken.js
@@ -1,51 +1,58 @@
 const {RecaptchaEnterpriseServiceClient} = require('@google-cloud/recaptcha-enterprise');
 
-// Crear una evaluación para analizar el riesgo de una acción de la IU
-async function createAssessment({ projectID, recaptchaKey, token, recaptchaAction }) {
-    const client = new RecaptchaEnterpriseServiceClient();
-    const projectPath = client.projectPath(projectID);
+/**
+  * Crea una evaluación para analizar el riesgo de una acción de la IU.
+  *
+  * projectID: El ID del proyecto de Google Cloud.
+  * recaptchaSiteKey: La clave reCAPTCHA asociada con el sitio o la aplicación
+  * token: El token generado obtenido del cliente.
+  * recaptchaAction: El nombre de la acción que corresponde al token.
+  */
+async function createAssessment({
+  // PENDIENTE: Reemplaza el token y las variables de acción de reCAPTCHA antes de ejecutar la muestra.
+  projectID = "posada-choroni-1728662692487",
+  recaptchaKey = "6Lc7tmUqAAAAANnpzULXy27w9h6nhkaqfS6CWZ7F",
+  token = "action-token",
+  recaptchaAction = "action-name",
+}) {
+  // Crea el cliente de reCAPTCHA.
+  // TODO: almacena en caché el código de generación de clientes (recomendado) o llama a client.close() antes de salir del método.
+  const client = new RecaptchaEnterpriseServiceClient();
+  const projectPath = client.projectPath(projectID);
 
-    const request = {
-        assessment: {
-            event: {
-                token: token,
-                siteKey: recaptchaKey,
-            },
-        },
-        parent: projectPath,
-    };
+  // Crea la solicitud de evaluación.
+  const request = ({
+    assessment: {
+      event: {
+        token: token,
+        siteKey: recaptchaKey,
+      },
+    },
+    parent: projectPath,
+  });
 
-    const [response] = await client.createAssessment(request);
+  const [ response ] = await client.createAssessment(request);
 
-    if (!response.tokenProperties.valid) {
-        console.log(`The CreateAssessment call failed because the token was: ${response.tokenProperties.invalidReason}`);
-        return null;
-    }
+  // Verifica si el token es válido.
+  if (!response.tokenProperties.valid) {
+    console.log(`The CreateAssessment call failed because the token was: ${response.tokenProperties.invalidReason}`);
+    return null;
+  }
 
-    if (response.tokenProperties.action === recaptchaAction) {
-        console.log(`The reCAPTCHA score is: ${response.riskAnalysis.score}`);
-        response.riskAnalysis.reasons.forEach((reason) => {
-            console.log(reason);
-        });
-        return response.riskAnalysis.score;
-    } else {
-        console.log("The action attribute in your reCAPTCHA tag does not match the action you are expecting to score");
-        return null;
-    }
-}
-
-const verifyRecaptchaToken = async (req, res, next) => {
-    const score = await createAssessment({
-        token: req.body['g-recaptcha-response'],
-        recaptchaAction: 'login',
+  // Verifica si se ejecutó la acción esperada.
+  // The `action` property is set by user client in the grecaptcha.enterprise.execute() method.
+  if (response.tokenProperties.action === recaptchaAction) {
+    // Obtén la puntuación de riesgo y los motivos.
+    // Para obtener más información sobre cómo interpretar la evaluación, consulta:
+    // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
+    console.log(`The reCAPTCHA score is: ${response.riskAnalysis.score}`);
+    response.riskAnalysis.reasons.forEach((reason) => {
+      console.log(reason);
     });
 
-    if (score === null || score < 0.5) {
-        return res.status(401).send('reCAPTCHA verification failed: low score');
-    }
-
-    req.recaptchaScore = score;
-    next();
-};
-
-module.exports = verifyRecaptchaToken;
+    return response.riskAnalysis.score;
+  } else {
+    console.log("The action attribute in your reCAPTCHA tag does not match the action you are expecting to score");
+    return null;
+  }
+}
